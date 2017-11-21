ec8d31e5e509d5a0b7e4cd234985ba4188082ec5
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-11-21 17:43:33 +0000

    Finished building a simple server using lobster

--- http_server.rb
+++ http_server.rb
@@ -1,14 +1,38 @@
 require 'socket'
+require 'rack'
+require 'rack/lobster'
+
+app = Rack::Lobster.new
+
+# Proc.new do
+#   ['200', {'Content-Type' => 'text/html'}, ['Hello world! The time is midnight']]
+# end
+
 server = TCPServer.new 5678
 
 while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200 \r\n"
-  session.print "Content-Type: text/html\r\n"
+  method, full_path = request.split(' ')
+
+  path, query = full_path.split('?')
+
+  status, headers, body = app.call({
+    'REQUEST_METHOD' => method,
+    'PATH_INFO' => path,
+    'QUERY_STRING' => query
+    })
+
+  session.print "HTTP/1.1 #{status} \r\n"
+  headers.each do |key, value|
+    session.print "#{key}: #{value}\r\n"
+  end
+
   session.print "\r\n"
-  session.print "Hello Lucy! The time now is not important"
+  body.each do |part|
+    session.print part
+  end
 
   session.close
 
